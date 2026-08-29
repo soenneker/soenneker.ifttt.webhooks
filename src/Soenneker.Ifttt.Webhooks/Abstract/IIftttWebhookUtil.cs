a9b22e7d458122f0e0839f9c247a0fe714971904
soenneker <jake@soenneker.com>
2026-08-29 14:17:07 -0500

    Improve public API XML documentation

--- src/Soenneker.Ifttt.Webhooks/Abstract/IIftttWebhookUtil.cs
+++ src/Soenneker.Ifttt.Webhooks/Abstract/IIftttWebhookUtil.cs
@@ -12,10 +12,10 @@ public interface IIftttWebhookUtil
     /// Triggers an IFTTT Webhooks event.
     /// </summary>
     /// <param name="eventName">The event name configured in the IFTTT applet.</param>
-    /// <param name="key">The IFTTT Webhooks key.</param>
-    /// <param name="value1">An optional first value supplied to the applet.</param>
-    /// <param name="value2">An optional second value supplied to the applet.</param>
-    /// <param name="value3">An optional third value supplied to the applet.</param>
+    /// <param name="key">Key used to locate the target entry.</param>
+    /// <param name="value1">Value for the trigger operation.</param>
+    /// <param name="value2">Value for the trigger operation.</param>
+    /// <param name="value3">Value for the trigger operation.</param>
     /// <param name="cancellationToken">A token that can cancel the request.</param>
     /// <returns>The response body returned by IFTTT.</returns>
     ValueTask<string> Trigger(string eventName, string key, string? value1 = null, string? value2 = null, string? value3 = null,
--- src/Soenneker.Ifttt.Webhooks/Registrars/IftttWebhookUtilRegistrar.cs
+++ src/Soenneker.Ifttt.Webhooks/Registrars/IftttWebhookUtilRegistrar.cs
@@ -13,6 +13,8 @@ public static class IftttWebhookUtilRegistrar
     /// <summary>
     /// Adds <see cref="IIftttWebhookUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddIftttWebhookUtilAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton().TryAddSingleton<IIftttWebhookUtil, IftttWebhookUtil>();
@@ -23,6 +25,8 @@ public static class IftttWebhookUtilRegistrar
     /// <summary>
     /// Adds <see cref="IIftttWebhookUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddIftttWebhookUtilAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton().TryAddScoped<IIftttWebhookUtil, IftttWebhookUtil>();
